707eb9c5a7f18727e7e78d928d869529d904f619
François Parmentier <francois.parmentier@gmail.com>
2019-11-06 11:48:30 +0100

    refactor: Remove useless string functions

--- src/strings.js
+++ src/strings.js
@@ -1,30 +1,10 @@
 import unidecode from 'unidecode';
-import { compose, includes, toLower } from 'ramda';
+import { compose, toLower } from 'ramda';
 
-const removeAccents = (str) => unidecode(str);
+export const removeAccents = (str) => unidecode(str);
 /**
  * Deplete string from accents and upper case.
  * @param {string} str
  * @returns {string}
  */
 export const depleteString = compose(toLower, removeAccents, String);
-
-/**
- * Check whether the lowercase of `haystack` includes the lowercase of `needle`.
- * @param {string} haystack
- * @param {string} needle
- * @returns {boolean}
- */
-export const includesDepleted = (haystack, needle) => includes(depleteString(needle), depleteString(haystack));
-
-/**
- * Check whether the `haystack` array lowercase elements includes the lowercase of `needle`.
- * @param {string[]} haystack
- * @param {string} needle
- * @returns {boolean}
- */
-export const includesDepletedArray = (haystack, needle) => (
-    haystack
-        .map(depleteString)
-        .includes(depleteString(needle))
-);
