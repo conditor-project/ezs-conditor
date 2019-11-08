1f89d567932b4cc76f6a4da34997c11082beb89c
François Parmentier <francois.parmentier@gmail.com>
2019-11-08 16:08:22 +0100

    feat: Add precision in findIdsInAddresses

--- bin/findIdsInAddresses.js
+++ bin/findIdsInAddresses.js
@@ -3,7 +3,7 @@
 
 const csv = require('csv-string');
 const { promises } = require('fs');
-const { intersection } = require('ramda');
+const { difference, intersection } = require('ramda');
 const { depleteString } = require('../lib/strings');
 const { isIn } = require('../lib/rnsr');
 const RNSR = require('../data/RNSR.json');
@@ -17,7 +17,10 @@ promises
         console.log('found\texpected\taddress');
         const rnsrAddresses = lines.slice(1);
         let expectedNb = 0;
-        const foundNb = rnsrAddresses.reduce((alreadyFound, rnsrAddress) => {
+        const { foundNb, wronglyFoundNb } = rnsrAddresses.reduce(({
+            foundNb: alreadyFound,
+            wronglyFoundNb: alreadyWronglyFound,
+        }, rnsrAddress) => {
             const depletedAddress = depleteString(rnsrAddress[1]);
             const isInAddress = isIn(depletedAddress);
             const rnsrIds = RNSR.structures.structure
@@ -31,10 +34,18 @@ promises
             if (foundIds.length < expectedIds.length) {
                 console.log(`${rnsrIds}\t${expectedIds}\t${depletedAddress}`);
             }
-            return alreadyFound + foundIds.length;
-        }, 0);
+            const wronglyFoundIds = difference(rnsrIds, expectedIds);
+            return {
+                foundNb: alreadyFound + foundIds.length,
+                wronglyFoundNb: alreadyWronglyFound + wronglyFoundIds.length,
+            };
+        }, { foundNb: 0, wronglyFoundNb: 0 });
         const recall = foundNb / expectedNb;
         console.log('recall:', recall);
         console.log('found:', foundNb);
         console.log('total:', expectedNb);
+        const precision = foundNb / (foundNb + wronglyFoundNb);
+        console.log('precision:', precision);
+        console.log('wrongs:', wronglyFoundNb);
+        console.log('total found:', foundNb + wronglyFoundNb);
     });
